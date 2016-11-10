33d9d0ba40b2b82647269ed80dc1f05a36a2cea7
fluoxa <fluoxa@googlemail.com>
2016-11-10 13:34:06 +0100

    erste Scheisse

--- src/main/java/de/baleipzig/configuration/strategies/ParentSelection.java
+++ src/main/java/de/baleipzig/configuration/strategies/ParentSelection.java
@@ -11,6 +11,46 @@ import java.util.function.Function;
 
 public class ParentSelection {
 
+    public static final Function<Population, Parents> RANDOM_PARENT_SELECTION =
+            population -> selectListOperation(population, individuals -> {
+                Collections.shuffle(individuals);
+                return individuals;
+        });
+
+
+    public static final Function<Population, Parents> DETERMINISTIC_PARENT_SELECTION =
+            population -> selectListOperation(population, individuals -> {
+                Collections.sort(individuals);
+                Long consideredIndividualCount = Math.round(individuals.size() * 0.2);
+                individuals = individuals.subList(0, consideredIndividualCount.intValue());
+                Collections.shuffle(individuals);
+                return individuals;
+            });
+
+    public static final Function<Population, Parents> MIXED_PARENT_SELECTION =
+            population -> {
+                double randomValue = Math.random();
+                if(randomValue < 0.5) {
+                    return DETERMINISTIC_PARENT_SELECTION.apply(population);
+                } else {
+                    return RANDOM_PARENT_SELECTION.apply(population);
+                }
+            };
+
+
+    private static Parents selectListOperation(Population population, Function<List<Individual>, List<Individual>> function) {
+        Parents parents = new Parents();
+
+        List<Individual> potentialParents = function.apply(population.getIndividuals());
+
+        Collections.sort(potentialParents, Collections.reverseOrder());
+
+        parents.setFather(potentialParents.get(0));
+        parents.setMother(potentialParents.get(1));
+        return parents;
+    }
+
+
     public static final Function<Population, Parents> FITTEST_PARENT_SELECTION = population -> {
         Parents parents = new Parents();
 
