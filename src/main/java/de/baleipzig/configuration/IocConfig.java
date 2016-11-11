29c7772832c5bc32f5f9749877ecbb3d4be66853
fluoxa <fluoxa@googlemail.com>
2016-11-11 12:37:57 +0100

    strats

--- src/main/java/de/baleipzig/configuration/IocConfig.java
+++ src/main/java/de/baleipzig/configuration/IocConfig.java
@@ -6,34 +6,41 @@ import de.baleipzig.genome.Genome;
 import de.baleipzig.individual.Individual;
 import de.baleipzig.individual.SuperHero;
 import de.baleipzig.population.Avengers;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class IocConfig {
 
     private final EvoConfig evoConfig;
 
-    private final Strategy strategy = new Strategy(ParentSelection.FITTEST_PARENT_SELECTION,
-            NaturalSelection.RANDOM_SELECTION,
-            Mutation.CHERNOBYL_MUTATION,
-            GenomeRecombination.INTERMEDIATE_RECOMBINATION);
+    private Strategy selectedStrategy;
+
+    private final Strategy constMutationRankSelection;
+
+    @Autowired
+    public IocConfig(EvoConfig evoConfig) {
+        this.evoConfig = evoConfig;
+
+        constMutationRankSelection = new Strategy(ParentSelection.RANDOM_PARENT_SELECTION,
+                NaturalSelection.RANK_RANDOM_SELECTION,
+                Mutation.getConstantRateMutationStrategy(evoConfig.getGenomeConfig()),
+                GenomeRecombination.INTERMEDIATE_RECOMBINATION);
+    }
 
     @Bean
     @Scope("prototype")
     public Avengers getPopulation() {
-        return new Avengers(evoConfig.getPopulationConfig(), strategy);
+        return new Avengers(evoConfig.getPopulationConfig(), selectedStrategy);
     }
 
     @Bean
     @Scope("prototype")
     public Individual getIndividual() {
 
-        return new SuperHero(FitnessFunction.griewankFitnessFactory(evoConfig.getGenomeConfig().getNumberOfGenes()), strategy);
+        return new SuperHero(FitnessFunction.griewankFitnessFactory(evoConfig.getGenomeConfig().getNumberOfGenes()), selectedStrategy);
     }
 
     @Bean
