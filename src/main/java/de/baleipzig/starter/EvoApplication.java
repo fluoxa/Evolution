831599028f1507e235ff50f36a1423a3dd1438a9
sonny <unknown>
2016-11-11 15:46:29 +0100

    abbruchkriterium hinzugefügt

--- src/main/java/de/baleipzig/starter/EvoApplication.java
+++ src/main/java/de/baleipzig/starter/EvoApplication.java
@@ -14,19 +14,23 @@ public class EvoApplication {
 
     private final EvoConfig evoConfig;
     private final ApplicationContext context;
-    private Population avengers;
 
     public void start() {
-        avengers = context.getBean(Population.class);
+
+        Population avengers = context.getBean(Population.class);
         avengers.populate();
 
         for (int cycle = 0; cycle < evoConfig.getEvolutionCycles(); cycle++) {
             avengers.createNextGeneration();
 
+            Statistic statistic = avengers.createStatistic();
 
             if(cycle % 10 == 0) {
-                Statistic statistic = avengers.createStatistic();
                 System.out.println(String.format("%s Fitness Mean: %.4f  +/-  %.4f  Best Fitness: %.4f", cycle, statistic.getMean(), statistic.getStandardDeviation(), statistic.getBestFitness()));
+            }
+
+            if(statistic.getStandardDeviation() < 0.001) {
+                break;
             }
         }
 
